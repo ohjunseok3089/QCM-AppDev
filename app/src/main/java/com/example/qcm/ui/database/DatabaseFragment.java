a0a7fb27dec1256bb0d54ed6ce3d6387122555c8
Junseok Oh <ohjunseok3089@gmail.com>
2023-02-24 19:10:28 -0500

    update

--- app/src/main/java/com/example/qcm/ui/database/DatabaseFragment.java
+++ app/src/main/java/com/example/qcm/ui/database/DatabaseFragment.java
@@ -9,24 +9,42 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.qcm.R;
 import com.example.qcm.databinding.FragmentDashboardBinding;
+import com.jjoe64.graphview.GraphView;
 
 public class DatabaseFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private RecyclerView recyclerView;
+//    private DatabaseAdapter adapter;
+//    private List<DatabaseItem> itemList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DatabaseViewModel databaseViewModel =
-                new ViewModelProvider(this).get(DatabaseViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View rootView = inflater.inflate(R.layout.fragment_database, container, false);
 
-        final TextView textView = binding.textDashboard;
-        databaseViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        // Find the RecyclerView in the layout
+//        recyclerView = rootView.findViewById(R.id.recycler_view);
+//
+//        // Create the list of database items
+//        itemList = new ArrayList<>();
+//        itemList.add(new DatabaseItem(R.drawable.thumbnail_1, "Title 1", "Date 1", "Type 1"));
+//        itemList.add(new DatabaseItem(R.drawable.thumbnail_2, "Title 2", "Date 2", "Type 2"));
+//        itemList.add(new DatabaseItem(R.drawable.thumbnail_3, "Title 3", "Date 3", "Type 3"));
+//        // Add more items as needed
+//
+//        // Create and set the adapter for the RecyclerView
+//        adapter = new DatabaseAdapter(itemList);
+//        recyclerView.setAdapter(adapter);
+//
+//        // Set the layout manager for the RecyclerView
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return rootView;
     }
 
     @Override
--- app/src/main/java/com/example/qcm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/qcm/ui/home/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.qcm.ui.home;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -28,6 +33,51 @@ public class HomeFragment extends Fragment {
 //        final TextView textView = binding.textHome;
         final TextView bluetoothDeviceName = binding.bluetoothDeviceName;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.newExp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                builder.setTitle("New Experiment");
+
+                // Set up the input
+                final EditText inputTitle = new EditText(requireContext());
+                final EditText inputSample = new EditText(requireContext());
+
+                // Set the input fields
+                inputTitle.setInputType(InputType.TYPE_CLASS_TEXT);
+                inputTitle.setHint("Experiment Title");
+
+                inputSample.setInputType(InputType.TYPE_CLASS_TEXT);
+                inputSample.setHint("Sample");
+
+                // Add the input fields to the dialog box
+                LinearLayout layout = new LinearLayout(requireContext());
+                layout.setOrientation(LinearLayout.VERTICAL);
+                layout.addView(inputTitle);
+                layout.addView(inputSample);
+                builder.setView(layout);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String title = inputTitle.getText().toString();
+                        String sample = inputSample.getText().toString();
+                        // Save the experiment with the given title and sample
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+
         return root;
     }
 
